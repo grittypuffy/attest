a5700915fae5e958ddd78ef27d82331e120846e2
vg006 <devvg006@gmail.com>
2026-01-13 00:49:46 +0530

    fix(web): Update baseURL in the api client

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,15 +1,9 @@
 import type { App } from "@api";
 import { treaty } from "@elysiajs/eden";
 
-<<<<<<< HEAD
-const baseURL = typeof window !== "undefined"
-  ? `${window.location.protocol}//${window.location.host}/api`
-  : "http://localhost:3000/api";
-=======
 const baseURL = "http://127.0.0.1:8000";
->>>>>>> onchain
 
-export const api = treaty<App>('http://localhost:8000', {
+export const api = treaty<App>(baseURL, {
   fetch: {
     credentials: "include",
   },
